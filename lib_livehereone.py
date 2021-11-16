7f3fd14527efd7723198a2141b6560d3d629bea7
Alberto <alberto.rinaudo+dino@gmail.com>
2021-11-16 23:36:59 +0000

    move janjua to dedicated import
    Signed-off-by: Alberto <alberto.rinaudo+dino@gmail.com>

--- lib_livehereone.py
+++ lib_livehereone.py
@@ -11,6 +11,8 @@ from addon import _handle
 from addon import add_directlink
 from addon import add_streamlink
 
+import fn_janjua as janjua
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0',
     'Referer': 'https://easysite.one/'
@@ -115,15 +117,9 @@ def add_links_rec(url_in, loop):
       j_channel = j_channel_re.group().split('\'')[1]
       j_g = j_g_re.group().split('\'')[1]
       if j_channel and j_g:
-        janjua_doc = requests.get("https://www.janjua.tv/hembedplayer/{0}/{1}/1920/1080".format(j_channel, j_g), headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0', 'Referer': 'https://www.janjua.tv/'}).text
-        janjua_video_re = re.search(r'enableVideo\("[^"]+', janjua_doc)
-        if janjua_video_re:
-          janjua_video = janjua_video_re.group().split('"')[1]
-          if janjua_video:
-            loadbalancer_doc = requests.get("https://www.tvportremote.com/loadbalancer").text
-            loadbalancer = loadbalancer_doc.split('=')[1]
-            if loadbalancer:
-              add_directlink("janjua", "https://{0}:8088/live/{1}/playlist.m3u8?id=1&pk={2}".format(loadbalancer, j_channel, janjua_video))
+        link_janjua = janjua.extract_link(j_channel, j_g)
+        if link_janjua:
+          add_directlink("janjua", link_janjua)
 
 
 def list_links(params):
